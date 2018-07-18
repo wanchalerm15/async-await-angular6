2fb2f94f2fa747ea127890200e9128f9af93cae7
WanchaloemMacBookPro <wanchaloemmacbookpro@WanchaloemMacBookPros-MacBook-Pro.local>
2018-07-18 07:29:39 +0700

    first async await project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
@@ -6,11 +7,32 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+    url: string = 'https://jsonplaceholder.typicode.com/posts';
+    items: any = [];
 
-    constructor() { }
+    constructor(private http: HttpClient) { }
 
-    ngOnInit() {
-
+    async ngOnInit() {
+        await this.getPosts();
+        console.log(this.items);
     }
 
+    async getPosts() {
+        this.items = await this.http.get(this.url).toPromise();
+        console.log(this.items);
+    }
+
+    syncFunction() {
+        confirm('Confirm!!!');
+        console.log('2 Sync Function');
+    }
+
+    asyncFunction() {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                console.log('2 Async Function');
+                resolve();
+            }, 100);
+        });
+    }
 }
